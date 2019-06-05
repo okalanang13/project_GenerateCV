04973455102d9b882d2b1e797484eb3fbdabaee3
falapuspita <fala.puspita@gmail.com>
2019-06-05 14:46:59 +0700

    updatelebaran

---
+++ src/main/java/bootcamp/mii/bootcamp/mii/controllers/CertificationController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bootcamp.mii.bootcamp.mii.controllers;
+
+import bootcamp.mii.bootcamp.mii.entities.Certification;
+import bootcamp.mii.bootcamp.mii.repositories.CertificationRepository;
+import bootcamp.mii.bootcamp.mii.services.CertificationService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author ASUS
+ */
+@Controller
+public class CertificationController {
+    @Autowired
+    private CertificationService certificationService;
+
+    @Autowired
+    private CertificationRepository certificationRepository;
+
+    @GetMapping("/certification")
+    public String certification(Model model) {
+        model.addAttribute("dataCertification", certificationService.findAllCertification());
+        return "certification";
+    }
+
+    @PostMapping("/addcertification")
+
+    public String addcertification(@Valid Certification c) {
+        c.setIsDelete('0');
+        certificationRepository.save(c);
+        return "redirect:/certification";
+
+    }
+
+    @GetMapping("/updatecertification")
+
+    public String updatecertification(@Valid Certification c) {
+        c.setIsDelete('0');
+        certificationRepository.save(c);
+        return "redirect:/certification";
+
+    }
+}
