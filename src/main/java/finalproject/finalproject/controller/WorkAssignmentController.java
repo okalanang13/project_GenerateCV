2ac82c16f55c7e3aab0e75247dc33bb4df15c535
RR17 <RR17@RR17>
2019-06-11 08:33:30 +0700

    CRUD WorkAss with input date and combo box

--- src/main/java/finalproject/finalproject/controller/WorkAssignmentController.java
+++ src/main/java/finalproject/finalproject/controller/WorkAssignmentController.java
@@ -6,7 +6,11 @@
 package finalproject.finalproject.controller;
 
 import finalproject.finalproject.entities.WorkAssignment;
+import finalproject.finalproject.repositories.EmployeeRepository;
+import finalproject.finalproject.repositories.JobRepository;
 import finalproject.finalproject.repositories.WorkAssignmentRepository;
+import finalproject.finalproject.services.EmployeeService;
+import finalproject.finalproject.services.JobService;
 import finalproject.finalproject.services.WorkAssignmentService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,18 +33,52 @@ public class WorkAssignmentController {
     @Autowired
     private WorkAssignmentService workAssignmentService;
     
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    
+    @Autowired
+    private EmployeeService employeeService;
+    
+    @Autowired
+    private JobRepository jobRepository;
+    
+    @Autowired
+    private JobService jobService;
+    
     //@GetMapping adalah anotasi tersusun yang berfungsi sebagai pintasan untuk 
     //@RequestMapping(method = RequestMethod.GET).
     //proses menampilkan semua data actor
     @GetMapping("/page/workAssignment")
     public String workAssignment(Model model) {
         model.addAttribute("dataWorkAssignment", workAssignmentService.getAllWorkAss());
+        model.addAttribute("dataEmployee", employeeService.getAllEmployee());
+        model.addAttribute("dataJob", jobService.getAllJob());
         return "workAssignment";
     }
     
     @PostMapping("/page/workAssignment/addDataWorkass")
     public String addDataWorkass (@Valid WorkAssignment workAssignment){
+        workAssignment.setId(0);
+        workAssignment.setIsDelete("false");
         workAssignmentRepository.save(workAssignment);
         return "redirect:/page/workAssignment";
+    }
+    
+    //proses update data dengan fungsi save dari Crud Repository
+    @PostMapping("/page/workAssignment/updateDataWorkass")
+    public String updateDataWorkass(@Valid WorkAssignment workAssignment) {
+        workAssignment.setIsDelete("false");
+        workAssignmentRepository.save(workAssignment);
+        return "redirect:/page/workAssignment";
+
+    }
+
+    //proses softdelete data dengan fungsi save dari Crud Repository
+    @GetMapping("/page/workAssignment/softDeleteDataWorkass")
+    public String softDeleteDataProvince(@Valid WorkAssignment workAssignment) {
+        workAssignment.setIsDelete("true");
+        workAssignmentRepository.save(workAssignment);
+        return "redirect:/page/workAssignment";
+
     }
 }
--- src/main/java/finalproject/finalproject/entities/WorkAssignment.java
+++ src/main/java/finalproject/finalproject/entities/WorkAssignment.java
@@ -24,6 +24,7 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  *
@@ -49,11 +50,13 @@ public class WorkAssignment implements Serializable {
     @Basic(optional = false)
     @NotNull
     @Column(name = "start_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Temporal(TemporalType.DATE)
     private Date startDate;
     @Basic(optional = false)
     @NotNull
     @Column(name = "end_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Temporal(TemporalType.DATE)
     private Date endDate;
     @Basic(optional = false)
